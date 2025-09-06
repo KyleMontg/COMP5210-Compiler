5230b9afaa8b0f72fb3db9efec6e99a599af4a29
KyleMontg <100159726+KyleMontg@users.noreply.github.com>
2025-09-05 21:34:41 -0500

    Lexer now works with list[str] and str. Added base plate for main

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
 #TODO Implement args, and run test if needed
+import argparse
+
+
+def create_arguments():
+    parser = argparse.ArgumentParser(
+    description = '' #TODO
+    )
+
+    # List tokens
+    parser.add_argument(
+        '-l',
+        "--list",
+        required=False,
+        help = 'Prints parsed tokens to the console'
+    )
+
+    # File location
+    parser.add_argument(
+        '-f',
+        "--file",
+        required=False,
+        help = 'Location of the file to be tokenized'
+    )
+
+    # Run Tests
+    parser.add_argument(
+        '-t',
+        "--test",
+        required=False,
+        help = 'Runs the default tests for tokenizer'
+    )
+
+    # Output file location
+    parser.add_argument(
+        '-o',
+        "--output",
+        required=False,
+        help = 'specifies where output file is generate. Defaults to local folder.'
+    )
+    return parser
+
+if __name__ == '__main__':
+    parser = create_arguments()
+    args = parser.parse_args()
--- test/test_lexer.py
+++ test/test_lexer.py
@@ -7,8 +7,14 @@ sys.path.append(src_file)
 #for x in enumerate(sys.path):
 #    print(x)
 import lexer
-with open(Path('./test/test_code/test1.txt')) as f:
+with open(Path('./test/test_code/test1.c')) as f:
     a = lexer.Tokenizer(f)
     tokens = a.tokenize()
     for x in tokens:
         print(x)
+
+
+def run_tests(dir_path: Path, print: bool):
+    test_path = Path(dir_path, '/test/test_code')
+    with open(test_path) as f:
+
